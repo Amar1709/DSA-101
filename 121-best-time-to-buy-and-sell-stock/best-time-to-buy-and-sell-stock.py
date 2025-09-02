59e1cf917de936b45838dcdbeeccccbccd50046d
Amar Parameswaran <55704780+Amar1709@users.noreply.github.com>
2025-09-02 22:30:56 +0530

    Time: 40 ms (79.85%) | Memory: 26.7 MB (99.27%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,10 @@ class Solution:
         maxP=0
         for i in range(1,len(prices)):
             P=prices[i]-minValSoFar
-            if P>maxP:
-                maxP=P
             if minValSoFar>prices[i]:
                 minValSoFar=prices[i]
+            elif P>maxP:
+                maxP=P
         return maxP
 
         
